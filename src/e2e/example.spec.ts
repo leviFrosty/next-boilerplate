bb1a70b226a594a8d015792ec15bc41da1f6f291
Levi Wilkerson <levi.wilkerson@proton.me>
2025-10-17 11:09:10 -0400

    fix(test): resolve example test

--- src/e2e/example.spec.ts
+++ src/e2e/example.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('/');
 
-  await expect(page).toHaveTitle('Create Next Web');
+  await expect(page).toHaveTitle('Create Next App');
 });
